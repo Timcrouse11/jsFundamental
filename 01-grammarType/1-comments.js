ace3418fe2a2933ef714104a435618c466052f86
Tim Crouse <tcrouse11@gmail.com>
2019-01-08 10:55:53 -0500

    first commitme

---
+++ 01-grammarType/1-comments.js
@@ -0,0 +1,13 @@
+// A comment! 
+//what comments do for us:
+//1) communicate with other developers by writing in plain English
+//2) 
+
+
+
+
+
+
+
+console.log('yay');
+// console.log('Tim Crouse');
---
+++ 01-grammarType/2-declarations.js
@@ -0,0 +1,26 @@
+var a = 1;
+
+var b = 1;
+/*(1) (2) (3) (4)
+
+1- Keyword
+2-
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*********** */
+
+var x;
+console.log('Declaration')
